7dce70936bfcf1d403336110b49c22b64987aca3
Jason Hendrickson <jason.hendrickson01@gmail.com>
2014-01-21 18:18:03 -0700

    Added ex9.py

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "\nMon\nTues\n Wed\nThu\nFri\nSat\nSun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+blah blah blah
+blew blew blew
+yeah yeah yeah
+no
+"""
